4ac2a4ea00ef6235d8742e1d8d3c3fce77c68e97
유병우 <conner903@naver.com>
2023-01-12 03:00:22 +0900

    💄: 모달 스타일 변경

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,6 +3,8 @@ import React, { useCallback, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { useHandleClickOutside } from "../../hooks";
 
+import "./index.scss";
+
 export interface ModalProps {
   visible: boolean;
   closeHandler: () => void;
@@ -36,9 +38,12 @@ const Modal: React.FC<ModalProps> = ({ visible, closeHandler, children }) => {
 
   return (
     <Portal>
-      <div />
-      <div ref={ref}>
-        <div>{children}</div>
+      <div className={`overlay-root ${visible ? "visble" : "invisible"}`} />
+      <div
+        className={`modal-root ${visible ? "visble" : "invisible"}`}
+        ref={ref}
+      >
+        <div className={`inner`}>{children}</div>
       </div>
       {/* <ModalOverlay isVisible={visible} />
       <ModalWrapper isVisible={visible} ref={ref}>
@@ -50,9 +55,3 @@ const Modal: React.FC<ModalProps> = ({ visible, closeHandler, children }) => {
   );
 };
 export default Modal;
-function useOnClickOutside(
-  ref: React.MutableRefObject<null>,
-  closeHandler: () => void
-) {
-  throw new Error("Function not implemented.");
-}
